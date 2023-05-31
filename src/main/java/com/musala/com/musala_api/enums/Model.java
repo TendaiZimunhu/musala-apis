b66936f332c37b447f9ebbc0c192dc0df850779c
TendaiZimunhu <zimunhutee@gmail.com>
2023-05-31 17:43:40 +0200

    main

---
+++ src/main/java/com/musala/com/musala_api/enums/Model.java
@@ -0,0 +1,5 @@
+package com.musala.com.musala_api.enums;
+
+public enum Model {
+    LIGHTWEIGHT, MIDDLEWEIGHT, CRUISERWEIGHT, HEAVYWEIGHT
+}
---
+++ src/main/java/com/musala/com/musala_api/enums/State.java
@@ -0,0 +1,10 @@
+package com.musala.com.musala_api.enums;
+
+public enum State {
+    IDLE,
+    LOADING,
+    LOADED,
+    DELIVERING,
+    DELIVERED,
+    RETURNING
+}
